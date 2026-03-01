2579b95ce309abcfa9a60a4966f9d9fa09271142
SpencerJ96 <spencer-jones1@hotmail.co.uk>
2026-03-01 16:27:22 +0000

    Lift city state to App and wiring up searchbar props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Header from './components/Header'
 import SearchBar from './components/SearchBar'
+import { useState } from 'react'
 
 function App() {
-  return (
+	const [searchedCity, setSearchedCity] = useState('')
+	console.log(searchedCity)
+	return (
 	<div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-600 pt-8">
     <Header />
-	<SearchBar />
+	<SearchBar onSearch={setSearchedCity} />
 	</div>
   )
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react' //Import UseState from React
 
-function SearchBar(){ //Component function for search bar
-	const [city, setCity] = useState('') //UseState a reaction function that sets up a piece of memory
+//SearchBar function that expects a prop (onSearch)
+//{TS part. onSearch is a function/prop that takes 1 argument (city thats a string and returns nothing)
+
+function SearchBar({ onSearch }: { onSearch: (city:string) => void}){ 
+	const [city, setCity] = useState('') //UseState a react function that sets up a piece of memory
 										 //And returns two things the current Value and an updater function
 
 
@@ -9,7 +12,9 @@ function SearchBar(){ //Component function for search bar
 		<div>{/* onChange watches for any typing in the input field, runs setCity
 			  with whatever is in the input box */}						
 			<input value={city} onChange={(e) => setCity(e.target.value)}/> 
-			<button onClick={() => console.log(city)}>Search</button>
+			{/*(setCity has updated city state*/}
+			{/* On click run our onSearch prop with the argument City (users value) */}
+			<button onClick={() => onSearch(city)}>Search</button>
 		</div>
 	)
 }
