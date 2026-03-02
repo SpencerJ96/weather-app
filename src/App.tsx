b29ac25c39e3ce78542845cbb53adb318bcd0a6a
SpencerJ96 <spencer-jones1@hotmail.co.uk>
2026-03-02 21:41:04 +0000

    Basic styling on searchbar comp

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { useState } from 'react'
 
 function App() {
 	const [searchedCity, setSearchedCity] = useState('')
-	console.log(searchedCity)
 	return (
 	<div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-600 pt-8">
     <Header />
+	{/* {city/userinput} from SearchBar.ts passed into {setSearchedCity} from the onSearch function
+	running in searchBar.ts 
+	 which stores the input into searchedCity apps state */}
 	<SearchBar onSearch={setSearchedCity} />
 	</div>
   )
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react' //Import UseState from React
 
-//SearchBar function that expects a prop (onSearch)
-//{TS part. onSearch is a function/prop that takes 1 argument (city thats a string and returns nothing)
 
-function SearchBar({ onSearch }: { onSearch: (city:string) => void}){ 
+//Prop the label of the package passed as component - Function whats inside the package
+//SearchBar function that expects a prop passed by App (onSearch)
+//TS part.
+// { onSearch } destructuring the prop from App "onSearch={setSearchedCity} "so we can use it 
+// this prop expects a parameter "city" and that parameter MUST Be a string and returns nothing
+// This catches errors so if someone enters intergers it throws an error 
+function SearchBar({ onSearch } : { onSearch: (city:string) => void}){ 
 	const [city, setCity] = useState('') //UseState a react function that sets up a piece of memory
 										 //And returns two things the current Value and an updater function
 
 
 	return( 					
-		<div>{/* onChange watches for any typing in the input field, runs setCity
+		<div className="flex justify-center p-8">{/* onChange watches for any typing in the input field, runs setCity
 			  with whatever is in the input box */}						
-			<input value={city} onChange={(e) => setCity(e.target.value)}/> 
+			<input className="px-4 py-2 rounded-l-xl w-64 outline-none text-gray-800 bg-white" value={city} onChange={(e) => setCity(e.target.value)}/> 
 			{/*(setCity has updated city state*/}
-			{/* On click run our onSearch prop with the argument City (users value) */}
-			<button onClick={() => onSearch(city)}>Search</button>
+			{/* On click run our onSearch prop with the argument City (updated users value) */}
+			<button className="px-4 py-2 rounded-r-xl bg-sky-500 hover:bg-sky-600 text-white font-semibold" onClick={() => onSearch(city)}>Search</button>
 		</div>
 	)
 }
