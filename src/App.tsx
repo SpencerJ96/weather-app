b3cecdc65a8d7460b10f33e38e0b9e370926da35
SpencerJ96 <spencer-jones1@hotmail.co.uk>
2026-03-02 22:21:35 +0000

    Add weather data fetch with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import Header from './components/Header'
 import SearchBar from './components/SearchBar'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
+	{/*State declarations */}
 	const [searchedCity, setSearchedCity] = useState('')
+	const [weatherData, setWeatherData] = useState(null)
+
+	useEffect(() => {
+		async function fetchWeather(){ if (!searchedCity) return
+			
+			const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`)
+			const data = await response.json()
+			setWeatherData(data)
+		}
+		fetchWeather()
+	}, [searchedCity])
+
 	return (
 	<div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-600 pt-8">
     <Header />
