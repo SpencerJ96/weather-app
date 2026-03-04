214d2b6f9aff6a90761298fe78376760053e70bc
SpencerJ96 <spencer-jones1@hotmail.co.uk>
2026-03-04 21:03:26 +0000

    Added Loading State

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,16 @@ function App() {
 	const [searchedCity, setSearchedCity] = useState('')
 	const [weatherData, setWeatherData] = useState(null)
 	const [errorMessage, setErrorMessage] = useState ('')
+	const [isLoading, setIsLoading] = useState (false)
 
 	//React Hook. 
 	useEffect(() => {
 		async function fetchWeather(){ if (!searchedCity) return
 			
+			setIsLoading(true)
 			const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`)
 			const data = await response.json()
+			setIsLoading(false)
 			
 			if (data.cod === "404") {
 				setErrorMessage ("Please Enter A Valid City")
@@ -27,7 +30,8 @@ function App() {
 		fetchWeather()
 	}, [searchedCity])
        //Listen For Changes To This State, When it Happens RUN THIS CODE. Dependency Array^^^ 
-
+	
+	   //JSX Expressions
 	return (
 	<div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-600 pt-8">
     <Header />
@@ -35,9 +39,12 @@ function App() {
 	running in searchBar.ts 
 	 which stores the input into searchedCity apps state */}
 	<SearchBar onSearch={setSearchedCity} />
+	{isLoading && <p className="text-sky-300 text-center px-2 py-2">Loading...</p>}
 	{ /* Conditional rendering.
 	 If the Left side is true, do it. If weatherData exists, make weather card */ }
 	{weatherData && <WeatherCard data = {weatherData} />}
+	{ /* Conditional rendering.
+	 If the Left side is true, do it. If errorMessage exists, make error text display*/ }
 	{errorMessage && <p className="font-bold text-red-500 text-center px-2 py-2">{errorMessage}</p>}
 	</div>
   )
